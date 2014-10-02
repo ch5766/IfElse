3257cdc21cdc45ffe0374754fbdd908a45e6ffef
ch5766 <choiwy2001@naver.com>
2014-10-02 16:32:10 +0900

    dzdz

--- sketch.js
+++ sketch.js
@@ -5,6 +5,21 @@ function setup() {
 }
 
 function draw() {
-   background(50);
+   background(150);
    ellipse(340, 220, mouseX, mouseY);
+   // draw function
+if (mouseX < 100) {
+  ellipse(50, 200, 50, 50);
+} else if (mouseX < 200){
+  ellipse(150, 200, 70, 70);
+} else if (mouseX < 300){
+  ellipse(250, 200, 80, 80);
+} else if (mouseX < 400){
+  ellipse(350, 200, 90, 90);
+} else if (mouseX < 500){
+  ellipse(450, 200, 100, 100);
+} else {
+  ellipse(550, 200, 150, 150);
+}
+
 }
